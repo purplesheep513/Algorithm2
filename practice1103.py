454731cfe381bb173ee9b3940b76c30901cef41b
purplesheep513 <prune1231@gmail.com>
2021-11-03 21:21:58 +0900

    마다마다다네

--- practice1103.py
+++ practice1103.py
@@ -17,3 +17,6 @@ def solution(numbers, target):
 numbers = [1,1,1,1,1]
 target = 3
 print(solution(numbers, target))
+
+# 생각해보면 간단한것인데 나는 +인경우 -인경우 두가지를 동시에 어떻게 실행하느냐에 집착하고있었다.
+# 그냥 계속 stack에 쌓고, 인덱스를 보내면 그 인덱스에 맞게 더해주므로 그렇게 생각하면 되는것이었다. 아직 멀었구나 난
