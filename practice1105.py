6816499409b321de80f952978e402f1be98b1c15
purplesheep513 <prune1231@gmail.com>
2021-11-06 00:02:26 +0900

    어렵네

--- practice1105.py
+++ practice1105.py
@@ -2,7 +2,7 @@ import sys
 arr = list(map(int, sys.stdin.readline().rstrip().split()))
 
 def solution(arr):
-    while arr[0] != arr[1] != arr[2] :
+    while arr[0]!=arr[1] and arr[0] != arr[2] and arr[1] != arr[2] :
         arr = sorted(arr)
         if arr[0] == arr[1] == arr[2] :
             return 1
@@ -11,7 +11,10 @@ def solution(arr):
         else : 
             arr[0], arr[2] = calculate_values(arr[0], arr[2])
             arr =[arr[0],arr[1],arr[2]]
-        
+    if arr[0] == arr[1] == arr[2] :
+        return 1
+    elif arr[0]==arr[1] or arr[0] == arr[2] or arr[1] == arr[2] :
+        return 0
 
 def calculate_values(var1,var2):
     return (var1+var1, var2-var1)
