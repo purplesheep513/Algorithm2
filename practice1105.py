1c64cade680e03358c0c1cc98b7bfaaef026eb01
purplesheep513 <prune1231@gmail.com>
2021-11-05 23:56:34 +0900

    아 왜 계속 논타입이지

---
+++ practice1105.py
@@ -0,0 +1,19 @@
+import sys
+arr = list(map(int, sys.stdin.readline().rstrip().split()))
+
+def solution(arr):
+    while arr[0] != arr[1] != arr[2] :
+        arr = sorted(arr)
+        if arr[0] == arr[1] == arr[2] :
+            return 1
+        elif arr[0]==arr[1] or arr[0] == arr[2] or arr[1] == arr[2] :
+            return 0
+        else : 
+            arr[0], arr[2] = calculate_values(arr[0], arr[2])
+            arr =[arr[0],arr[1],arr[2]]
+        
+
+def calculate_values(var1,var2):
+    return (var1+var1, var2-var1)
+
+print(solution(arr))
