51599ed225b424a161e65a6cc2f240c72a705a96
purplesheep513 <prune1231@gmail.com>
2022-02-11 17:20:00 +0900

    graph[x][y] 값을 1로 설정하지 않아 recursion오류가 났었음..

--- practice0211.py
+++ practice0211.py
@@ -7,6 +7,7 @@ def dfs(x, y):
     if x <= -1 or x >= N or y <= -1 or y >= M:
         return False
     if graph[x][y] == 0:
+        graph[x][y] = 1
         dfs(x-1, y)
         dfs(x, y-1)
         dfs(x+1, y)
@@ -20,3 +21,5 @@ for i in range(N):
     for j in range(M):
         if dfs(i, j) == True:
             result += 1
+
+print(result)
