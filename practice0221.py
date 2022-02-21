cc107c7c75e123be71573418c66c41ed21762b97
purplesheep513 <prune1231@gmail.com>
2022-02-21 22:54:57 +0900

    이진탐색 다시

---
+++ practice0221.py
@@ -0,0 +1,19 @@
+m = 6
+arr = [19, 15, 10, 17]
+
+start = 0
+end = max(arr)
+
+result = 0
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+    for x in arr:
+        if x > mid:
+            total += x - mid
+
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
